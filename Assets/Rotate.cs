9cccb7fffea7b7cffc031058e66b1271fe226fd5
Daan de Graaf <daandegraaf9@gmail.com>
2018-06-03 01:00:12 +0200

    Rotating drone motor

---
+++ Assets/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody rb = GetComponent<Rigidbody>();
+		rb.angularVelocity = new Vector3(0, 500f, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
