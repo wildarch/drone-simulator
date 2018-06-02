66b0f9f4e7fcfaf14bf06ea1514b04286f0e5954
Daan de Graaf <daandegraaf9@gmail.com>
2018-06-03 00:50:51 +0200

    Initial source commit

---
+++ Assets/PropNoise.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropNoise : MonoBehaviour {
+	public float noiseLevel = 1.0f;
+
+	private Rigidbody parentRb;
+
+	// Use this for initialization
+	void Start () {
+		parentRb = (Rigidbody) GetComponentInParent(typeof(Rigidbody));
+	}
+	
+	void FixedUpdate () {
+		Vector3 noise = new Vector3(
+			noiseLevel*(Random.value-0.5f), 
+			noiseLevel*(Random.value-0.5f), 
+			noiseLevel*(Random.value-0.5f)
+		);
+		parentRb.AddForceAtPosition(noise, transform.parent.up);
+	}
+}
