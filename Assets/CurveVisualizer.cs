9f21df33c520b8cb68107ec5c3c97725114d6b3a
Daan de Graaf <daandegraaf9@gmail.com>
2018-06-09 16:15:56 +0200

    Path visualization and basic path following

---
+++ Assets/CurveVisualizer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurveVisualizer : MonoBehaviour {
+
+	public int sections = 10;
+
+	private BezierCurve curve;
+	private LineRenderer lineRenderer;
+
+	// Use this for initialization
+	void Start () {
+		curve = GetComponent<BezierCurve>();
+		lineRenderer = GetComponent<LineRenderer>();
+		float delta = 1f/sections;
+		Vector3[] points = new Vector3[sections + 1];
+
+		float t = 0;
+		for(int i = 0; i < points.Length; i++) {
+			points[i] = curve.GetPointAt(t);
+			t += delta;
+		}
+		lineRenderer.positionCount = sections + 1;
+		lineRenderer.SetPositions(points);
+	}
+}
---
+++ Assets/PathFollower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollower : MonoBehaviour {
+	public float pathTotalTime = 5;
+	public Transform targetIndicator;
+	
+	private BezierCurve curve;
+
+	void Start () {
+		curve = GetComponent<BezierCurve>();
+	}
+	
+	void FixedUpdate () {
+		float t = (Time.timeSinceLevelLoad / pathTotalTime) % 1f;
+		Vector3 target = curve.GetPointAt(t);
+		targetIndicator.position = target;
+	}
+}
